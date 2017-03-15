ed6f961226820d9de7e158d15805ecb880ecabf5
rflechner <rflechner@romcyber.com>
2017-03-15 21:29:38 +0100

    fix build

--- src/Suave.Swagger/AssemblyInfo.fs
+++ src/Suave.Swagger/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Suave.Swagger")>]
 [<assembly: AssemblyProductAttribute("Suave.Swagger")>]
 [<assembly: AssemblyDescriptionAttribute("This is an extension for Suave.io with some REST tools and Swagger documentation helpers")>]
-[<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
+[<assembly: AssemblyVersionAttribute("1.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0"
-    let [<Literal>] InformationalVersion = "1.0.0"
+    let [<Literal>] Version = "1.0.1"
+    let [<Literal>] InformationalVersion = "1.0.1"
