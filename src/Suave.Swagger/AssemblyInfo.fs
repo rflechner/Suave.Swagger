9efab95402f1f52bb1dc56b5af57903803095226
rflechner <rflechner@romcyber.com>
2017-07-27 12:03:12 +0200

    TASK: docs

--- src/Suave.Swagger/AssemblyInfo.fs
+++ src/Suave.Swagger/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Suave.Swagger")>]
 [<assembly: AssemblyProductAttribute("Suave.Swagger")>]
 [<assembly: AssemblyDescriptionAttribute("This is an extension for Suave.io with some REST tools and Swagger documentation helpers")>]
-[<assembly: AssemblyVersionAttribute("2.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.1")>]
+[<assembly: AssemblyVersionAttribute("2.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Suave.Swagger"
     let [<Literal>] AssemblyProduct = "Suave.Swagger"
     let [<Literal>] AssemblyDescription = "This is an extension for Suave.io with some REST tools and Swagger documentation helpers"
-    let [<Literal>] AssemblyVersion = "2.0.1"
-    let [<Literal>] AssemblyFileVersion = "2.0.1"
+    let [<Literal>] AssemblyVersion = "2.1.0"
+    let [<Literal>] AssemblyFileVersion = "2.1.0"
